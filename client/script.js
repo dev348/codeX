641e16391df4a09a05a4633ae3b982708d4f62e4
Meenakshisundareswaran R <m.sundareswaran@amazecodes.com>
2023-02-15 17:32:57 +0530

    add new url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server 
-  const response = await fetch('http://localhost:3000',{
+  const response = await fetch('https://codex-oxft.onrender.com',{
     method:'POST',
     headers:{
         'Content-Type':'application/json'
